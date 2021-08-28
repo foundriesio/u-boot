arduino/u-boot-imx

commit e92f55f271d556653871bd81931186fbcec95b51

[FIO toup] mmc: fsl_esdhc_imx: initialize data for imx7ulp
Changes done as per the linux kernel.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

drivers/mmc/fsl_esdhc_imx.c
@@ -1672,6 +1672,17 @@ static struct esdhc_soc_data usdhc_imx8qm_data = {
 		ESDHC_FLAG_HS400 | ESDHC_FLAG_HS400_ES,
 };
 
+#define ESDHC_FLAG_STATE_LOST_IN_LPMODE BIT(10)
+#define ESDHC_FLAG_PMQOS		BIT(13)
+
+static struct esdhc_soc_data usdhc_imx7ulp_data = {
+	.flags = ESDHC_FLAG_USDHC | ESDHC_FLAG_STD_TUNING
+			| ESDHC_FLAG_HAVE_CAP1 | ESDHC_FLAG_HS200
+			| ESDHC_FLAG_HS400
+			| ESDHC_FLAG_STATE_LOST_IN_LPMODE
+			| ESDHC_FLAG_PMQOS,
+};
+
 static const struct udevice_id fsl_esdhc_ids[] = {
 	{ .compatible = "fsl,imx53-esdhc", },
 	{ .compatible = "fsl,imx6ul-usdhc", },
@@ -1679,7 +1690,7 @@ static const struct udevice_id fsl_esdhc_ids[] = {
 	{ .compatible = "fsl,imx6sl-usdhc", },
 	{ .compatible = "fsl,imx6q-usdhc", },
 	{ .compatible = "fsl,imx7d-usdhc", .data = (ulong)&usdhc_imx7d_data,},
-	{ .compatible = "fsl,imx7ulp-usdhc", },
+	{ .compatible = "fsl,imx7ulp-usdhc", .data = (ulong)&usdhc_imx7ulp_data,},
 	{ .compatible = "fsl,imx8qm-usdhc", .data = (ulong)&usdhc_imx8qm_data,},
 	{ .compatible = "fsl,imx8mm-usdhc", .data = (ulong)&usdhc_imx8qm_data,},
 	{ .compatible = "fsl,imx8mn-usdhc", .data = (ulong)&usdhc_imx8qm_data,},

foundriesio/u-boot

commit 1b18f6ddbeb9df68576875fd21d5ff43cd9b62bd

[FIO toup-squash] mmc: fsl_esdhc_imx: fix ESDHC_FLAG_STATE_LOST_IN_LPMODE definition
In the Linux kernel, the 10th bit of esdhc flags tell that the sdhci
controller has errata ERR010450 (which presents on iMX6ULL only).
The correct bit for telling that "The IP state got lost in low power
mode" is 14th.
Fix the definition of ESDHC_FLAG_STATE_LOST_IN_LPMODE to avoid confusing
with linux kernel definition.

Fixes: e92f55f271 ("[FIO toup] mmc: fsl_esdhc_imx: initialize data for imx7ulp")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/mmc/fsl_esdhc_imx.c
@@ -1672,8 +1672,8 @@ static struct esdhc_soc_data usdhc_imx8qm_data = {
 		ESDHC_FLAG_HS400 | ESDHC_FLAG_HS400_ES,
 };
 
-#define ESDHC_FLAG_STATE_LOST_IN_LPMODE BIT(10)
 #define ESDHC_FLAG_PMQOS		BIT(13)
+#define ESDHC_FLAG_STATE_LOST_IN_LPMODE BIT(14)
 
 static struct esdhc_soc_data usdhc_imx7ulp_data = {
 	.flags = ESDHC_FLAG_USDHC | ESDHC_FLAG_STD_TUNING