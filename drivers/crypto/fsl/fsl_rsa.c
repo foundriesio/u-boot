arduino/u-boot-imx

commit 6b0c6d738367644a8c13aae303caf1d48cd4a3c0

[FIO toup] DM: crypto/fsl: fix alignment issue in fsl_rsa driver
fdt_getprop() may allocate and return an unaligned property value.
Using it on other PU may cause crash [1].

Align the public_exponent property to fix the issue.

[1]
---
Trying to boot from MMC2
    sha256,rsa2048:dev"Synchronous Abort" handler, esr 0x96000021
    elr: 00000000007e737c lr : 00000000007e6e78
    x 0: 00000000007fd1e4 x 1: 0000000000000100
    x 2: 0000000000000800 x 3: 000000000091d420
    x 4: 000000000091d420 x 5: 000000000091d588
    x 6: 00000000401ff9b8 x 7: 0000000000000003
    x 8: 000000000091d49c x 9: 0000000000000002
...
    Code: f94008a0 2a0103f4 aa0303f5 b40005c0 (f9400003)
    Resetting CPU ...
---

Fixes: 34276478f7 ("DM: crypto/fsl - Add Freescale rsa DM driver")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/crypto/fsl/fsl_rsa.c
@@ -23,6 +23,7 @@ int fsl_mod_exp(struct udevice *dev, const uint8_t *sig, uint32_t sig_len,
 	struct pk_in_params pkin;
 	uint32_t desc[MAX_CAAM_DESCSIZE];
 	int ret;
+	fdt64_t exp = fdt64_to_cpu(*prop->public_exponent);
 
 	/* Length in bytes */
 	keylen = prop->num_bits / 8;
@@ -31,7 +32,7 @@ int fsl_mod_exp(struct udevice *dev, const uint8_t *sig, uint32_t sig_len,
 	pkin.a_siz = sig_len;
 	pkin.n = prop->modulus;
 	pkin.n_siz = keylen;
-	pkin.e = prop->public_exponent;
+	pkin.e = (void *)&exp;
 	pkin.e_siz = prop->exp_len;
 
 	inline_cnstr_jobdesc_pkha_rsaexp(desc, &pkin, out, sig_len);

foundriesio/u-boot

commit 6c6a7b126b7c5501e5cc203e971093289e02fd5b

[FIO toup-squash] DM: crypto/fsl: fix bogus dereferencing
Fix bogus dereferencing causing the compiler error [1].

[1]
drivers/crypto/fsl/fsl_rsa.c:26:29: warning: dereferencing ‘void *’ pointer
   26 |  fdt64_t exp = fdt64_to_cpu(*prop->public_exponent);
      |                             ^~~~~~~~~~~~~~~~~~~~~~
...
include/linux/byteorder/little_endian.h:39:50: error: invalid use of void expression
   39 | #define __be64_to_cpu(x) __swab64((__force __u64)(__be64)(x))
      |                                                  ^
---------------------------------------------------------------------

Fixes: 6b0c6d7383 ("[FIO toup] DM: crypto/fsl: fix alignment issue in fsl_rsa driver")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/crypto/fsl/fsl_rsa.c
@@ -23,7 +23,7 @@ int fsl_mod_exp(struct udevice *dev, const uint8_t *sig, uint32_t sig_len,
 	struct pk_in_params pkin;
 	uint32_t desc[MAX_CAAM_DESCSIZE];
 	int ret;
-	fdt64_t exp = fdt64_to_cpu(*prop->public_exponent);
+	fdt64_t exp = fdt64_to_cpu(prop->public_exponent);
 
 	/* Length in bytes */
 	keylen = prop->num_bits / 8;