arduino/u-boot-imx

commit b6b1a255abed6fca833ecbe31544660891e653f9

[FIO internal] imx8mm_evk: fix build issues when SPL_DM=y
1. Force disable DM_I2C for the SPL build (workaround)
2. Disable usage of TCPC from the board file for the SPL build, as
from board/freescale/common/Makefile:

ifndef CONFIG_SPL_BUILD
obj-$(CONFIG_USB_TCPC) += tcpc.o
endif

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

board/freescale/imx8mm_evk/imx8mm_evk.c
@@ -121,7 +121,7 @@ int board_phy_config(struct phy_device *phydev)
 }
 #endif
 
-#ifdef CONFIG_USB_TCPC
+#if defined(CONFIG_USB_TCPC) && !defined(CONFIG_SPL_BUILD)
 struct tcpc_port port1;
 struct tcpc_port port2;
 
@@ -302,9 +302,9 @@ int board_init(void)
 {
 	struct arm_smccc_res res;
 
-#ifdef CONFIG_USB_TCPC
-	setup_typec();
-#endif
+	if (IS_ENABLED(CONFIG_USB_TCPC) &&
+	   !IS_ENABLED(CONFIG_SPL_BUILD))
+		setup_typec();
 
 	if (IS_ENABLED(CONFIG_FEC_MXC))
 		setup_fec();

include/configs/imx8mm_evk.h
@@ -38,6 +38,7 @@
 #else
 #define CONFIG_POWER_BD71837
 #endif
+#undef CONFIG_DM_I2C
 
 #define CONFIG_SYS_I2C
 

foundriesio/u-boot

commit 3ed15fe1530d621f213fdadc6be69ada4a717842

[FIO internal-squash] imx8mm_evk: fix parsing undeclared function
The setup_typec() declares only on particular circumstances. This
issue has no consequences but a compiler warning.
Use the same clauses to avoid building warning [1].

[1]
board/freescale/imx8mm_evk/imx8mm_evk.c: In function ‘board_init’:
board/freescale/imx8mm_evk/imx8mm_evk.c:307:3: warning: implicit declaration of function ‘setup_typec’;
...

Fixes: b6b1a255ab ("[FIO internal] imx8mm_evk: fix build issues when SPL_DM=y")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

board/freescale/imx8mm_evk/imx8mm_evk.c
@@ -302,9 +302,9 @@ int board_init(void)
 {
 	struct arm_smccc_res res;
 
-	if (IS_ENABLED(CONFIG_USB_TCPC) &&
-	   !IS_ENABLED(CONFIG_SPL_BUILD))
-		setup_typec();
+#if defined(CONFIG_USB_TCPC) && !defined(CONFIG_SPL_BUILD)
+	setup_typec();
+#endif
 
 	if (IS_ENABLED(CONFIG_FEC_MXC))
 		setup_fec();