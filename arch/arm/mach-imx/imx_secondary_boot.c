arduino/u-boot-imx

commit 06dd9179ee8232f7ed84fa179c8555fa34581d96

[FIO internal] imx: add imx_warm_reset cmd
Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/imx_secondary_boot.c
@@ -59,3 +59,21 @@ U_BOOT_CMD(
 	"   0 - set primary image\n"
 	"   1 - set secondary image\n"
 );
+
+static int do_warm_reset(cmd_tbl_t *cmdtp, int flag,
+			 int argc, char * const argv[])
+{
+	unsigned long ret = 0;
+
+	ret = call_imx_sip(IMX_SIP_WARM_RESET, 0, 0, 0, 0);
+	if (ret)
+		return CMD_RET_FAILURE;
+
+	return CMD_RET_SUCCESS;
+}
+
+U_BOOT_CMD(
+	imx_warm_reset, CONFIG_SYS_MAXARGS, 1, do_warm_reset,
+	"Assert WDOG Software Reset Signal (internal reset) via TF-A",
+	"\n"
+);

include/imx_sip.h
@@ -30,4 +30,6 @@
 #define IMX_SIP_FIPS_CONFIG		0xC200000D
 #define IMX_SIP_FIPS_CONFIG_SET		0x1
 
+#define IMX_SIP_WARM_RESET	0xC2000010
+
 #endif

foundriesio/u-boot

commit 4d878b458f413c16bfba81a11acc3a542ceeda28

[FIO internal-squash] imx: secondary_boot: switch to smccc calls for warm_reset
Use new generic wrappers for doing SMC calls instead of old legacy
NXP-specific call_imx_sip().

Fixes: 06dd9179ee ("[FIO internal] imx: add imx_warm_reset cmd")
Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/imx_secondary_boot.c
@@ -88,11 +88,7 @@ U_BOOT_CMD(
 static int do_warm_reset(struct cmd_tbl *cmdtp, int flag,
 			 int argc, char * const argv[])
 {
-	unsigned long ret = 0;
-
-	ret = call_imx_sip(IMX_SIP_WARM_RESET, 0, 0, 0, 0);
-	if (ret)
-		return CMD_RET_FAILURE;
+	arm_smccc_smc(IMX_SIP_WARM_RESET, 0, 0, 0, 0, 0, 0, 0, NULL);
 
 	return CMD_RET_SUCCESS;
 }