arduino/u-boot-imx

commit dd9336eccb20dda766afecf249ca1ab07747a293

[FIO fromlist] spl: Add CONFIG_SPL_FIT_SIGNATURE_STRICT
SPL FIT load checks the signature on loadable images but just continues
in the case of a failure. This is undesirable behavior because the boot
process depends on the authenticity of each loadable part.

Adding CONFIG_SPL_FIT_SIGNATURE_STRICT to halt the platform when any
image fails its signature check, including loadable parts.

SPL already supports image signature verification but had no mechanism
to check that the FIT's configuration block was signed correctly.

Add a check near the start of spl_load_simple_fit that verifies the
FIT's configuration block, and fails if it's not present or the
signature doesn't match what's stored in the SPL DTB.

Signed-off-by: Henry Beberman <hebeberm@microsoft.com>
Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

common/spl/spl_fit.c
@@ -330,7 +330,12 @@ static int spl_load_fit_image(struct spl_load_info *info, ulong sector,
 		printf("## Checking hash(es) for Image %s ... ",
 		       fit_get_name(fit, node, NULL));
 		if (!fit_image_verify_with_data(fit, node, src, length))
-			return -EPERM;
+			if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
+				puts("Invalid FIT signature found in a required image.\n");
+				hang();
+			} else {
+				return -EPERM;
+			}
 		puts("OK\n");
 	}
 
@@ -627,6 +632,20 @@ int spl_load_simple_fit(struct spl_image_info *spl_image,
 	ret = spl_simple_fit_read(&ctx, info, sector, fit);
 	if (ret < 0)
 		return ret;
+
+	if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
+		int cfg_noffset = fit_conf_get_node(fit, NULL);
+
+		if (cfg_noffset >= 0) {
+			if (fit_config_verify(fit, cfg_noffset)) {
+				puts("Unable to verify the required FIT config.\n");
+				hang();
+			}
+		} else {
+			puts("SPL_FIT_SIGNATURE_STRICT needs a config node in FIT\n");
+			hang();
+		}
+	}
 
 	/* skip further processing if requested to enable load-only use cases */
 	if (spl_load_simple_fit_skip_processing())

common/Kconfig.boot
@@ -191,6 +191,13 @@ config SPL_FIT_SIGNATURE
 	select SPL_IMAGE_SIGN_INFO
 	select SPL_FIT_FULL_CHECK
 
+config SPL_FIT_SIGNATURE_STRICT
+	bool "Halt if loadables or firmware don't pass FIT signature verification"
+	select SPL_FIT_SIGNATURE
+	help
+	  Strictly requires each loadable or firmware in a FIT image to be
+	  passed verification. Halt if any loadable fails to be verified.
+
 config SPL_LOAD_FIT
 	bool "Enable SPL loading U-Boot as a FIT (basic fitImage features)"
 	select SPL_FIT

foundriesio/u-boot

commit c1907f22d4ec60b7fdfbf8a166d079bb6304cb62

[FIO extras] fit: verify: abort if signature not found and strict
Prefer aborting the fit verification process when a signature node is
not found by U-Boot and FIT SIGNATURE STRICT is enabled. This is a way
to always force a proper signature verification via sig verify.

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

[FIO toup-squash] spl: minor fixes for FIT_SIGNATURE_STRICT mode

This fixes a possible ambiguous 'else' [2] and using undeclared
function hang() [1].

[1]
common/spl/spl_fit.c: In function ‘spl_load_fit_image’:
common/spl/spl_fit.c:335:5: warning: implicit declaration of function ‘hang’ [-Wimplicit-function-declaration]
  335 |     hang();
      |     ^~~~
---------------------------------------------------------------------
[2]
common/spl/spl_fit.c:332:6: warning: suggest explicit braces to avoid ambiguous ‘else’ [-Wdangling-else]
  332 |   if (!fit_image_verify_with_data(fit, node, src, length))
      |      ^
---------------------------------------------------------------------
Fixes: dd9336eccb ("[FIO fromlist] spl: Add CONFIG_SPL_FIT_SIGNATURE_STRICT")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

[FIO toup-squash] spl: use spl_simple_fit_parse for STRICT validation

FIT image is already parsed and verified by the spl_simple_fit_parse
function, so instead of searching for the default config node entry and
validating its signature, just use the return code from the fit parser
and hang in case of errors.

Fixes: dd9336eccb ("[FIO fromlist] spl: Add CONFIG_SPL_FIT_SIGNATURE_STRICT")
Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

common/image-fit-sig.c
@@ -15,6 +15,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #endif /* !USE_HOSTCC*/
 #include <fdt_region.h>
 #include <image.h>
+#include <linux/kconfig.h>
 #include <u-boot/rsa.h>
 #include <u-boot/hash-checksum.h>
 
@@ -197,9 +198,15 @@ int fit_image_verify_required_sigs(const void *fit, int image_noffset,
 	*no_sigsp = 1;
 	sig_node = fdt_subnode_offset(sig_blob, 0, FIT_SIG_NODENAME);
 	if (sig_node < 0) {
-		debug("%s: No signature node found: %s\n", __func__,
-		      fdt_strerror(sig_node));
-		return 0;
+		if (IS_ENABLED(CONFIG_FIT_SIGNATURE_STRICT)) {
+			printf("%s: No signature node found: %s\n", __func__,
+			       fdt_strerror(sig_node));
+			return -1;
+		} else {
+			debug("%s: No signature node found: %s\n", __func__,
+			      fdt_strerror(sig_node));
+			return 0;
+		}
 	}
 
 	fdt_for_each_subnode(noffset, sig_blob, sig_node) {
@@ -429,9 +436,15 @@ static int fit_config_verify_required_sigs(const void *fit, int conf_noffset,
 	/* Work out what we need to verify */
 	sig_node = fdt_subnode_offset(sig_blob, 0, FIT_SIG_NODENAME);
 	if (sig_node < 0) {
-		debug("%s: No signature node found: %s\n", __func__,
-		      fdt_strerror(sig_node));
-		return 0;
+		if (IS_ENABLED(CONFIG_FIT_SIGNATURE_STRICT)) {
+			printf("%s: No signature node found: %s\n", __func__,
+			       fdt_strerror(sig_node));
+			return -1;
+		} else {
+			debug("%s: No signature node found: %s\n", __func__,
+			      fdt_strerror(sig_node));
+			return 0;
+		}
 	}
 
 	/* Get required-mode policy property from DTB */

common/image-sig.c
@@ -19,6 +19,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #include <u-boot/ecdsa.h>
 #include <u-boot/rsa.h>
 #include <u-boot/hash-checksum.h>
+#include <linux/kconfig.h>
 
 #define IMAGE_MAX_HASHED_NODES		100
 

common/spl/spl_fit.c
@@ -17,6 +17,7 @@
 #include <asm/cache.h>
 #include <asm/global_data.h>
 #include <linux/libfdt.h>
+#include <hang.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -314,13 +315,14 @@ static int spl_load_fit_image(struct spl_load_info *info, ulong sector,
 	if (CONFIG_IS_ENABLED(FIT_SIGNATURE)) {
 		printf("## Checking hash(es) for Image %s ... ",
 		       fit_get_name(fit, node, NULL));
-		if (!fit_image_verify_with_data(fit, node, src, length))
+		if (!fit_image_verify_with_data(fit, node, src, length)) {
 			if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
 				puts("Invalid FIT signature found in a required image.\n");
 				hang();
 			} else {
 				return -EPERM;
 			}
+		}
 		puts("OK\n");
 	}
 
@@ -684,27 +686,19 @@ int spl_load_simple_fit(struct spl_image_info *spl_image,
 	if (ret < 0)
 		return ret;
 
-	if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
-		int cfg_noffset = fit_conf_get_node(fit, NULL);
-
-		if (cfg_noffset >= 0) {
-			if (fit_config_verify(fit, cfg_noffset)) {
-				puts("Unable to verify the required FIT config.\n");
-				hang();
-			}
-		} else {
-			puts("SPL_FIT_SIGNATURE_STRICT needs a config node in FIT\n");
-			hang();
-		}
-	}
-
 	/* skip further processing if requested to enable load-only use cases */
 	if (spl_load_simple_fit_skip_processing())
 		return 0;
 
 	ret = spl_simple_fit_parse(&ctx);
-	if (ret < 0)
-		return ret;
+	if (ret < 0) {
+		if (CONFIG_IS_ENABLED(FIT_SIGNATURE_STRICT)) {
+			puts("SPL_FIT_SIGNATURE_STRICT needs a valid config node in FIT\n");
+			hang();
+		} else {
+			return ret;
+		}
+	}
 
 	if (IS_ENABLED(CONFIG_SPL_FPGA))
 		spl_fit_load_fpga(&ctx, info, sector);