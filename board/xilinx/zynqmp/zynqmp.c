foundriesio/u-boot

commit 09d96c0d9e8c164a1adb0acb5e77af61a337c7e9

[FIO toup] zynqmp: multi_boot cmd valid value check
Add additional check to multi_boot cmd implementation, so it permits to set
values of multiboot offset register that correspond to
SYS_SPI_BOOT_IMAGE_OFFS/SYS_SPI_BOOT_IMAGE_OFFS2.

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

board/xilinx/zynqmp/zynqmp.c
@@ -413,6 +413,22 @@ static int do_multi_boot(struct cmd_tbl *cmdtp, int flag,
 		printf("Incorrect value of multiboot offset\n");
 		return CMD_RET_USAGE;
 	}
+#if defined(CONFIG_SPL_SPI_SUPPORT)
+	if (!strcmp(env_get("modeboot"), "qspiboot")) {
+		u32 boot_image_offset = golden_image_boundary * multiboot;
+
+		if (boot_image_offset != CONFIG_SYS_SPI_BOOT_IMAGE_OFFS &&
+		    boot_image_offset != CONFIG_SYS_SPI_BOOT_IMAGE_OFFS2) {
+			printf("Invalid value of multiboot register"
+			       "supported values are: %d, %d\n",
+			       (CONFIG_SYS_SPI_BOOT_IMAGE_OFFS /
+				golden_image_boundary),
+			       (CONFIG_SYS_SPI_BOOT_IMAGE_OFFS2 /
+				golden_image_boundary));
+			return CMD_RET_FAILURE;
+		}
+	}
+#endif
 
 	printf("Set multiboot offset to: \t%d\n", multiboot);
 

commit 692672e923d8391a2a43409176158425e20b2bbb

[FIO toup-squash] zynqmp: do_multi_boot: fix error output
Fix error output print.

Fixes: 09d96c0d9("[FIO toup] zynqmp: multi_boot cmd valid value check")
Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

board/xilinx/zynqmp/zynqmp.c
@@ -419,7 +419,7 @@ static int do_multi_boot(struct cmd_tbl *cmdtp, int flag,
 
 		if (boot_image_offset != CONFIG_SYS_SPI_BOOT_IMAGE_OFFS &&
 		    boot_image_offset != CONFIG_SYS_SPI_BOOT_IMAGE_OFFS2) {
-			printf("Invalid value of multiboot register"
+			printf("Invalid value of multiboot register, "
 			       "supported values are: %d, %d\n",
 			       (CONFIG_SYS_SPI_BOOT_IMAGE_OFFS /
 				golden_image_boundary),