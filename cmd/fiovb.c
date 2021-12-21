arduino/u-boot-imx

commit 545d964536327abdd3c0b9e6272d80b49b0737e2

[FIO extras] fiovb: return error on init if board not closed
Signed-off-by: Tim Anderson <tim.anderson@foundries.io>

cmd/fiovb.c
@@ -20,6 +20,9 @@ int do_fiovb_init(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[]
 	if (argc != 2)
 		return CMD_RET_USAGE;
 
+	if (!boot_mode_is_closed())
+		return CMD_RET_FAILURE;
+
 	mmc_dev = simple_strtoul(argv[1], NULL, 16);
 
 	if (fiovb_ops)

arch/arm/mach-imx/misc.c
@@ -213,3 +213,8 @@ int add_res_mem_dt_node(void *fdt, const char *name, phys_addr_t pa,
 	}
 	return 0;
 }
+
+int __weak boot_mode_is_closed(void)
+{
+	return 0;
+}

foundriesio/u-boot

commit 7c6ae3b71e3a2317df073f3b4f57c04359e029c8

[FIO internal-squash] cmd: fiovb: add include for boot_mode_is_closed
Add include sys_proto.h, where boot_mode_is_closed() is defined.

This fixes this compile time warning:
cmd/fiovb.c:23:7: warning: implicit declaration of function 'boot_mode_is_closed' [-Wimplicit-function-declaration]
23 |  if (!boot_mode_is_closed())
   |       ^~~~~~~~~~~~~~~~~~~

Fixes: 545d964536 ("[FIO extras] fiovb: return error on init if board not closed")
Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
(cherry-picked from 26f9df8e614550ffacc5327384d95ff97045349f)
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

cmd/fiovb.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 #include <common.h>
+#include <asm/arch/sys_proto.h>
 #include <command.h>
 #include <env.h>
 #include <image.h>