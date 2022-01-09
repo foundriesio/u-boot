arduino/u-boot-imx

commit 37f8fc5c109940c4c893e02d0ce29bb5822bc3bd

[FIO extras] mx6ullevk: drop MODULE_FUSE and OF_SYSTEM_SETUP for SPL
MODULE_FUSE adds ~8K of binary size to SPL where it's only used for 1
check to see if USB has been fused off.

For FIO use-case we only enable USB for mfgtool, where USB fusing
would not apply.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

include/configs/mx6ullevk.h
@@ -294,6 +294,9 @@
 #define CONFIG_VIDEO_BMP_RLE8
 #define CONFIG_VIDEO_BMP_LOGO
 #endif
+#else /* for SPL_BUILD disable these subsystems */
+#undef CONFIG_MODULE_FUSE
+#undef CONFIG_OF_SYSTEM_SETUP
 #endif
 
 #endif

foundriesio/u-boot

commit 023d74398cc1fc0d149929b940ff566a4f36650b

[FIO extras-squash] mx6ullevk: drop undefining MODULE_FUSE
In u-boot 2021.04 the selecting of IMX_MODULE_FUSE and OF_SYSTEM_SETUP
was disabled on Kconfig level. Drop obsolete undefining.

Fixes: 37f8fc5c10 ("[FIO extras] mx6ullevk: drop MODULE_FUSE and OF_SYSTEM_SETUP for SPL")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

include/configs/mx6ullevk.h
@@ -293,9 +293,6 @@
 #define CONFIG_VIDEO_BMP_RLE8
 #define CONFIG_VIDEO_BMP_LOGO
 #endif
-#else /* for SPL_BUILD disable these subsystems */
-#undef CONFIG_MODULE_FUSE
-#undef CONFIG_OF_SYSTEM_SETUP
 #endif
 
 #endif