bf7b6823e55d8cc75d72c72290bc8e73b4b41cc8
Igor Opaniuk <igor.opaniuk@foundries.io>
2022-06-23 16:59:51 +0300

    [FIO toup] stm32mp: read auth stats and boot_partition from tamp
    Obtain from TAMP backup register information about image authorization
    status and partition used for booting.
    Store this info in env variables (boot_auth and boot_part correspondingly).
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- arch/arm/mach-stm32mp/include/mach/sys_proto.h
+++ arch/arm/mach-stm32mp/include/mach/sys_proto.h
@@ -47,6 +47,9 @@ void get_soc_name(char name[SOC_NAME_SIZE]);
 /* return boot mode */
 u32 get_bootmode(void);
 
+/* return auth status and partition */
+u32 get_bootauth(void);
+
 int setup_mac_address(void);
 
 /* board power management : configure vddcore according OPP */
