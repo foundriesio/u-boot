foundriesio/u-boot

commit dcf2c6b6d2b2d83e32ff2b7ef9f93d0dbed162d6

fpga: zynqmp: support loading authenticated images
Add supporting new compatible string "u-boot,zynqmp-fpga-ddrauth" to
handle loading authenticated images (DDR).

Based on solution by Jorge Ramirez-Ortiz <jorge@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
Series-changes: 10
- Support DDR images only if FPGA_LOAD_SECURE enabled.

drivers/fpga/zynqmppl.c
@@ -9,6 +9,7 @@
 #include <common.h>
 #include <compiler.h>
 #include <cpu_func.h>
+#include <fpga.h>
 #include <log.h>
 #include <zynqmppl.h>
 #include <zynqmp_firmware.h>
@@ -232,11 +233,28 @@ static int zynqmp_load(xilinx_desc *desc, const void *buf,
 	u32 buf_lo, buf_hi;
 	u32 bsize_req = (u32)bsize;
 	u32 ret_payload[PAYLOAD_ARG_CNT];
-
+#if CONFIG_IS_ENABLED(FPGA_LOAD_SECURE)
+	struct fpga_secure_info info = { 0 };
+#endif
 	debug("%s called!\n", __func__);
 
 	if (zynqmp_check_compatible(desc, flags)) {
 		puts("Missing loads operation or unsupported bitstream type\n");
+		return FPGA_FAIL;
+	}
+
+	switch (flags) {
+	case FPGA_LEGACY:
+		break;	/* Handle the legacy image later in this function */
+#if CONFIG_IS_ENABLED(FPGA_LOAD_SECURE)
+	case FPGA_XILINX_ZYNQMP_DDRAUTH:
+		/* DDR authentication */
+		info.authflag = ZYNQMP_FPGA_AUTH_DDR;
+		info.encflag = FPGA_NO_ENC_OR_NO_AUTH;
+		return desc->operations->loads(desc, buf, bsize, &info);
+#endif
+	default:
+		puts("Unsupported bitstream type\n");
 		return FPGA_FAIL;
 	}
 
@@ -334,7 +352,10 @@ static int zynqmp_str2flag(xilinx_desc *desc, const char *str)
 {
 	if (!strncmp(str, "u-boot,fpga-legacy", 18))
 		return FPGA_LEGACY;
-
+#if CONFIG_IS_ENABLED(FPGA_LOAD_SECURE)
+	if (!strncmp(str, "u-boot,zynqmp-fpga-ddrauth", 26))
+		return FPGA_XILINX_ZYNQMP_DDRAUTH;
+#endif
 	return 0;
 }
 

boot/Kconfig
@@ -209,8 +209,8 @@ config SPL_LOAD_FIT
 	  1. "loadables" images, other than FDTs, which do not have a "load"
 	     property will not be loaded. This limitation also applies to FPGA
 	     images with the correct "compatible" string.
-	  2. For FPGA images, only the "compatible" = "u-boot,fpga-legacy"
-	     loading method is supported.
+	  2. For FPGA images, the supported "compatible" list is in the
+	     doc/uImage.FIT/source_file_format.txt.
 	  3. FDTs are only loaded for images with an "os" property of "u-boot".
 	     "linux" images are also supported with Falcon boot mode.
 

include/xilinx.h
@@ -39,6 +39,7 @@ typedef enum {			/* typedef xilinx_family */
 
 /* FPGA bitstream supported types */
 #define FPGA_LEGACY			BIT(0)
+#define FPGA_XILINX_ZYNQMP_DDRAUTH	BIT(1)
 
 typedef struct {		/* typedef xilinx_desc */
 	xilinx_family family;	/* part type */

include/zynqmppl.h
@@ -25,6 +25,10 @@
 
 extern struct xilinx_fpga_op zynqmp_op;
 
+#if CONFIG_IS_ENABLED(FPGA_LOAD_SECURE)
+#define ZYNQMP_FPGA_FLAGS	(FPGA_LEGACY | FPGA_XILINX_ZYNQMP_DDRAUTH)
+#else
 #define ZYNQMP_FPGA_FLAGS	(FPGA_LEGACY)
+#endif
 
 #endif /* _ZYNQMPPL_H_ */

commit 1c8838f910ee9d63b3e3c53eb30448ab92f63d2f

fpga: zynqmp: fix handling legacy image
Fix treating an incoming FPGA image with empty flags parameter as
legacy.

Fixes:  dcf2c6b6d2b ("fpga: zynqmp: support loading authenticated images")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/fpga/zynqmppl.c
@@ -202,9 +202,12 @@ static int zynqmp_validate_bitstream(xilinx_desc *desc, const void *buf,
 
 static int zynqmp_check_compatible(xilinx_desc *desc, int flags)
 {
-	/* If no flags set, the image is legacy */
+	/*
+	 * If no flags set, the image may be legacy, but we need to
+	 * signal caller this situation with specific eror code.
+	 */
 	if (!flags)
-		return 0;
+		return -ENODATA;
 
 	/* For legacy bitstream images no need for other methods exist */
 	if ((flags & desc->flags) && flags == FPGA_LEGACY)
@@ -219,7 +222,7 @@ static int zynqmp_check_compatible(xilinx_desc *desc, int flags)
 	    (flags & desc->flags))
 		return 0;
 
-	return FPGA_FAIL;
+	return -ENODEV;
 }
 
 static int zynqmp_load(xilinx_desc *desc, const void *buf,
@@ -238,9 +241,14 @@ static int zynqmp_load(xilinx_desc *desc, const void *buf,
 #endif
 	debug("%s called!\n", __func__);
 
-	if (zynqmp_check_compatible(desc, flags)) {
-		puts("Missing loads operation or unsupported bitstream type\n");
-		return FPGA_FAIL;
+	ret = zynqmp_check_compatible(desc, flags);
+	if (ret) {
+		if (ret != -ENODATA) {
+			puts("Missing loads operation or unsupported bitstream type\n");
+			return FPGA_FAIL;
+		}
+		/* If flags is not set, the image treats as legacy */
+		flags = FPGA_LEGACY;
 	}
 
 	switch (flags) {
@@ -259,7 +267,7 @@ static int zynqmp_load(xilinx_desc *desc, const void *buf,
 		return desc->operations->loads(desc, buf, bsize, &info);
 #endif
 	default:
-		puts("Unsupported bitstream type\n");
+		printf("Unsupported bitstream type %d\n", flags);
 		return FPGA_FAIL;
 	}
 