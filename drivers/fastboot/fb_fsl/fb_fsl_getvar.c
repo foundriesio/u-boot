foundriesio/u-boot

commit bd6722adf89065c17be984cfc6a2bd14b14e3290

[FIO tonxp] fastboot: fix build warnings
Fix the following annoying build warnings:

drivers/fastboot/fb_fsl/fb_fsl_common.c: In function ‘fastboot_setup’:
drivers/fastboot/fb_fsl/fb_fsl_common.c:359:2: warning: ‘serialnr.high’ is used uninitialized in this function [-Wuninitialized]
  359 |  sprintf(serial, "%08x%08x", serialnr.high, serialnr.low);
      |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
drivers/fastboot/fb_fsl/fb_fsl_common.c:359:2: warning: ‘serialnr.low’ is used uninitialized in this function [-Wuninitialized]
drivers/fastboot/fb_fsl/fb_fsl_getvar.c:117:13: warning: ‘serial’ defined but not used [-Wunused-variable]
  117 | static char serial[IMX_SERIAL_LEN];
      |             ^~~~~~

Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
(cherry picked from commit f2c35562c77c0d224417199a44a128d831b027e5)
(cherry picked from commit c95e406d28d811de32e14e101cf7c13f95ec51fb)
Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -118,7 +118,9 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
+#ifdef CONFIG_SERIAL_TAG
 static char serial[IMX_SERIAL_LEN];
+#endif
 
 char *get_serial(void)
 {

drivers/fastboot/fb_fsl/fb_fsl_common.c
@@ -368,7 +368,10 @@ static int _fastboot_setup_dev(int *switched)
 void fastboot_setup(void)
 {
 	int sw, ret;
-	struct tag_serialnr serialnr;
+	struct tag_serialnr serialnr = {
+		.high = 0,
+		.low = 0,
+	};
 	char serial[17];
 
 	if (!env_get("serial#")) {

commit 946009edcc509a642f2ea175cb27fac9041d7c45

[FIO tosquash] fastboot: correctly fix build warning
serial usage depends on two configs, so make sure they are both covered
at the variable declaration to avoid build failures.

Fixes: bd6722adf89 ("[FIO tonxp] fastboot: fix build warnings")

Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -118,7 +118,7 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
-#ifdef CONFIG_SERIAL_TAG
+#if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
 static char serial[IMX_SERIAL_LEN];
 #endif
 