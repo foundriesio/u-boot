foundriesio/u-boot

commit 9af5f2d5c60e170fe25cc1301c272169dafc8290

[FIO toup] imx8m: ddr: Reduce DDR4/LPDDR4 DMEM firmware padding
DMEM firmware files are padded to 16KB, wasting precious space
in processor internal RAM (TMU). The actual size of these files
is less than 2KB, so padding them to PAGE_SIZE is sufficient.

This patch changes the expected size of DMEM firmware.
It needs to change boot image creation tools to generate 4KB padding.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/ddr/imx/phy/helper.c
@@ -17,8 +17,12 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #define IMEM_LEN 32768 /* byte */
+#if CONFIG_IS_ENABLED(IMX8M)
+#define DMEM_LEN 4096 /* byte */
+#else
 #define DMEM_LEN 16384 /* byte */
-#define IMEM_2D_OFFSET	49152
+#endif
+#define IMEM_2D_OFFSET (IMEM_LEN + DMEM_LEN)
 
 #define IMEM_OFFSET_ADDR 0x00050000
 #define DMEM_OFFSET_ADDR 0x00054000

commit bd6df65c9335a081069ff87cf61efde9d31f65d9

[FIO squash] imx8m: ddr: fix changing padding
CONFIG_IS_ENABLED($option) checks for CONFIG_SPL_$option when
building SPL, whereas SPL_IMX8M is not exist.

We should have changed padding in SPL. Fix checking IMX8M family for
SPL.

Fixes:   9af5f2d5c60 ("[FIO toup] imx8m: ddr: Reduce DDR4/LPDDR4 DMEM firmware padding")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/ddr/imx/phy/helper.c
@@ -17,7 +17,7 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #define IMEM_LEN 32768 /* byte */
-#if CONFIG_IS_ENABLED(IMX8M)
+#if IS_ENABLED(CONFIG_IMX8M)
 #define DMEM_LEN 4096 /* byte */
 #else
 #define DMEM_LEN 16384 /* byte */