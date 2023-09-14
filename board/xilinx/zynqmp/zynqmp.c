foundriesio/u-boot

commit cb96e38af4acba7eeb39b396cc8a1a3a57c2ed7f

[FIO toup] zynqmp: add dynamic detection of fit image spi offset
Add dynamic detection of FIT image offset for QSPI boot.

Testing (regular boot from primary boot image):
U-Boot SPL 2021.07+xlnx+gc5d8da6b06 (Nov 02 2021 - 21:49:46 +0000)
PMUFW:	v1.1
Loading new PMUFW cfg obj (2168 bytes)
Silicon version:	3
EL Level:	EL3
Multiboot:	0
Trying to boot from SPI
SPL: Booting next image from 0x100000 SPI offset

Testing (booting secondary boot image):
U-Boot SPL 2021.07+xlnx+g3f451431d6 (Nov 02 2021 - 22:09:42 +0000)
PMUFW:	v1.1
Loading new PMUFW cfg obj (2168 bytes)
Silicon version:	3
EL Level:	EL3
Multiboot:	10
Trying to boot from SPI
SPL: Booting next image from 0xaa0000 SPI offset

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

board/xilinx/zynqmp/zynqmp.c
@@ -1010,6 +1010,40 @@ void set_dfu_alt_info(char *interface, char *devstr)
 }
 #endif
 
+#if defined(CONFIG_SPL_SPI_SUPPORT)
+unsigned int spl_spi_get_uboot_offs(struct spi_flash *flash)
+{
+	int ret;
+	u32 multiboot;
+	u32 spi_offset = 0;
+
+	multiboot = multi_boot_get();
+
+	/*
+	 * Primary boot.bin offset   - 0x0 (multiboot == 0)
+	 * Secondary boot.bin offset - 0x50000 (multiboot == 10,
+	 *                             as 10 * 32KB == 0x50000)
+	 */
+	switch(multiboot) {
+	case 0:
+		spi_offset = CONFIG_SYS_SPI_U_BOOT_OFFS;
+		break;
+	case 10:
+		spi_offset = CONFIG_SYS_SPI_U_BOOT_OFFS2;
+		break;
+	default:
+		printf("Invalid value of multiboot register, value = %d\n",
+		       multiboot);
+		hang();
+		break;
+	}
+
+	printf("SPL: Booting next image from 0x%x SPI offset\n", spi_offset);
+
+	return spi_offset;
+}
+#endif
+
 #if defined(CONFIG_SPL_FS_LOAD_PAYLOAD_NAME)
 int spl_mmc_get_uboot_payload_filename(char *filename, size_t len,
 				       const u32 boot_device)

arch/arm/mach-zynqmp/Kconfig
@@ -24,6 +24,14 @@ config SPL_SPI_FLASH_SUPPORT
 config SPL_SPI
 	default y if ZYNQ_QSPI
 
+config SYS_SPI_U_BOOT_OFFS2
+	default 0xaa0000
+	hex "address of secondary u-boot payload in SPI flash"
+	depends on SPL_SPI_LOAD
+	help
+	  Address within SPI-Flash from where the u-boot secondary payload
+	  is fetched from.
+
 config SYS_BOARD
 	string "Board name"
 	default "zynqmp"

configs/xilinx_zynqmp_virt_defconfig
@@ -7,6 +7,7 @@ CONFIG_SYS_MALLOC_F_LEN=0x8000
 CONFIG_SYS_MEMTEST_START=0x00000000
 CONFIG_SYS_MEMTEST_END=0x00001000
 CONFIG_SYS_SPI_U_BOOT_OFFS=0x100000
+CONFIG_SYS_SPI_U_BOOT_OFFS2=0xaa0000
 CONFIG_DM_GPIO=y
 CONFIG_DEFAULT_DEVICE_TREE="zynqmp-zcu100-revC"
 CONFIG_SPL_STACK_R_ADDR=0x18000000

commit 28464ff46f448742c5c7cd04d8d574440739ecbb

[FIO toup] zynqmp: use primary offset by default
Use CONFIG_SYS_SPI_U_BOOT_OFFS instead of hang() if boot.bin was booted
from unsupported multiboot offset.

Fixes: cb96e38 ("[FIO toup] zynqmp: add dynamic detection of fit image spi offset")
Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

board/xilinx/zynqmp/zynqmp.c
@@ -1080,14 +1080,10 @@ unsigned int spl_spi_get_uboot_offs(struct spi_flash *flash)
 	 * Secondary boot.bin offset - 0x50000 (multiboot == 10,
 	 *                             as 10 * 32KB == 0x50000)
 	 */
-	if (boot_image_offset == CONFIG_SYS_SPI_BOOT_IMAGE_OFFS) {
-		payload_offset = CONFIG_SYS_SPI_U_BOOT_OFFS;
-	} else if (boot_image_offset == CONFIG_SYS_SPI_BOOT_IMAGE_OFFS2) {
+	if (boot_image_offset == CONFIG_SYS_SPI_BOOT_IMAGE_OFFS2) {
 		payload_offset = CONFIG_SYS_SPI_U_BOOT_OFFS2;
 	} else {
-		printf("Invalid value of multiboot register, value = %d\n",
-		       multiboot);
-		hang();
+		payload_offset = CONFIG_SYS_SPI_U_BOOT_OFFS;
 	}
 
 	printf("SPL: Booting next image from 0x%x SPI offset\n",