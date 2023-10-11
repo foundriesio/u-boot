foundriesio/u-boot

commit ccd549f5a8fd7309ad76c3bb86e47074a45d811e

[FIO internal] ARM: mach-imx: spl: add support of imx8mn_evk in spl_mmc_get_uboot_raw_sector()
iMX8MN can have 2 mmc drives. Add support of booting from 2nd drive
to spl_mmc_get_uboot_raw_sector().
Remove an incomplete version of that function from imx8mn_evk/spl.c.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/spl.c
@@ -554,7 +554,11 @@ unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
 {
 	int boot_secondary = boot_mode_getprisec();
 	unsigned long offset = CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
-
+#if defined(CONFIG_IMX8MN)
+	#define UBOOT_MMC2_RAW_SECTOR_OFFSET 0x40
+	if (spl_boot_device() == BOOT_DEVICE_MMC2)
+			offset -= UBOOT_MMC2_RAW_SECTOR_OFFSET;
+#endif
 	if (boot_secondary) {
 		offset += CONFIG_SECONDARY_BOOT_SECTOR_OFFSET;
 		printf("SPL: Booting secondary boot path: using 0x%lx offset "

board/freescale/imx8mn_evk/spl.c
@@ -215,18 +215,3 @@ void board_init_f(ulong dummy)
 
 	board_init_r(NULL, 0);
 }
-
-#ifdef CONFIG_SPL_MMC
-#define UBOOT_RAW_SECTOR_OFFSET 0x40
-unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc)
-{
-	u32 boot_dev = spl_boot_device();
-	switch (boot_dev) {
-		case BOOT_DEVICE_MMC1:
-			return CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
-		case BOOT_DEVICE_MMC2:
-			return CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR - UBOOT_RAW_SECTOR_OFFSET;
-	}
-	return CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
-}
-#endif

commit 32cab455601c6083213ebd02221e8694955a28e9

[FIO internal] imx8mn: spl: remove adjusting start sector for MMC2
The NXP commit [1] and its LmP version [2] break booting LmP. Remove the
adjustment of start sector for MMC2 to fix LmP booting.

[1]
commit c72a447592b ("MA-15087-4 Support mmc loader for imx8mn_evk")

[2]
commit  ccd549f5a8f ("[FIO internal] ARM: mach-imx: spl: add support of imx8mn_evk in spl_mmc_get_uboot_raw_sector()")

Fixes: ccd549f5a8f ("[FIO internal] ARM: mach-imx: spl: add support of imx8mn_evk in spl_mmc_get_uboot_raw_sector()")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/spl.c
@@ -554,11 +554,6 @@ unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
 {
 	int boot_secondary = boot_mode_getprisec();
 	unsigned long offset = CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
-#if defined(CONFIG_IMX8MN)
-	#define UBOOT_MMC2_RAW_SECTOR_OFFSET 0x40
-	if (spl_boot_device() == BOOT_DEVICE_MMC2)
-			offset -= UBOOT_MMC2_RAW_SECTOR_OFFSET;
-#endif
 	if (boot_secondary) {
 		offset += CONFIG_SECONDARY_BOOT_SECTOR_OFFSET;
 		printf("SPL: Booting secondary boot path: using 0x%lx offset "