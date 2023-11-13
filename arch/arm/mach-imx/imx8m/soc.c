63055c06ec19b79ec6038216b1fd5c81ce30dac6
Igor Opaniuk <igor.opaniuk@foundries.io>
2023-11-13 18:42:28 +0100

    [FIO internal] imx8m: use existing code for partition detection
    Use existing implementation of spl_mmc_emmc_boot_partition()
    for primary/secondary boot detection for imx8mn and imx8mp.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -723,8 +723,7 @@ int arch_initr_trap(void)
 struct rom_api *g_rom_api = (struct rom_api *)0x980;
 #endif
 
-#if !defined(CONFIG_SECONDARY_BOOT_RUNTIME_DETECTION) && \
-    defined(CONFIG_IMX8M)
+#if defined(CONFIG_IMX8MN) || defined(CONFIG_IMX8MP)
 #include <spl.h>
 int spl_mmc_emmc_boot_partition(struct mmc *mmc)
 {
@@ -783,6 +782,17 @@ int spl_mmc_emmc_boot_partition(struct mmc *mmc)
 	}
 
 	return part;
+}
+unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
+					   unsigned long raw_sect)
+{
+	unsigned long offset = CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
+#if defined(CONFIG_iMX8MN)
+	#define UBOOT_MMC2_RAW_SECTOR_OFFSET 0x40
+	if (spl_boot_device() == BOOT_DEVICE_MMC2)
+			offset -= UBOOT_MMC2_RAW_SECTOR_OFFSET;
+#endif
+	return offset;
 }
 #endif
 
