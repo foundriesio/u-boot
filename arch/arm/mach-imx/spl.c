foundriesio/u-boot

commit adf2fc975a387b57a6db4a865ddd0f2b1bf6529a

[FIO toup] arch: mach-imx: hab: add SPL_IMX_HAB config
This allows granularity of turning off HAB features for SPL
which might not need them.

Upstream-Status: Pending
Signed-off-by: Michael Scott <mike@foundries.io>
Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/spl.c
@@ -254,7 +254,7 @@ u32 spl_mmc_boot_mode(struct mmc *mmc, const u32 boot_device)
 }
 #endif
 
-#if defined(CONFIG_IMX_HAB)
+#if defined(CONFIG_SPL_IMX_HAB)
 
 /*
  * +------------+  0x0 (DDR_UIMAGE_START) -
@@ -526,7 +526,7 @@ int board_spl_fit_post_load(const void *fit, struct spl_image_info *spl_image)
 	int ret;
 #endif
 
-	if (IS_ENABLED(CONFIG_IMX_HAB) && !(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD)) {
+	if (CONFIG_IS_ENABLED(IMX_HAB) && !(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD)) {
 		u32 offset = ALIGN(fdt_totalsize(fit), 0x1000);
 
 		if (imx_hab_authenticate_image((uintptr_t)fit,

arch/arm/mach-imx/Kconfig
@@ -84,9 +84,17 @@ config IMX_HAB
 	  This option enables the support for secure boot (HAB).
 	  See doc/imx/habv4/* for more details.
 
+config SPL_IMX_HAB
+	bool "Support i.MX HAB features in SPL"
+	default y if IMX_HAB
+	depends on ARCH_MX7 || ARCH_MX6 || ARCH_MX5 || ARCH_MX7ULP || ARCH_IMX8M
+	help
+	  This option enables the support for secure boot (HAB) in SPL.
+	  See doc/README.mxc_hab for more details.
+
 config IMX_SPL_FIT_FDT_SIGNATURE
 	bool "Enable to verify signature of FIT FDT"
-	depends on IMX_HAB
+	depends on SPL_IMX_HAB
 	depends on ARCH_IMX8M
 	help
 	  Enable SPL to verify signature of FIT FDT during FIT loading.

arch/arm/mach-imx/Makefile
@@ -18,7 +18,7 @@ obj-y += mmc_env.o
 obj-$(CONFIG_FEC_MXC) += mac.o
 obj-$(CONFIG_DWC_ETH_QOS) += mac.o
 obj-$(CONFIG_SYS_I2C_MXC) += i2c-mxv7.o
-obj-$(CONFIG_IMX_HAB) += hab.o
+obj-$(CONFIG_$(SPL_)IMX_HAB) += hab.o
 obj-y += cpu.o
 endif
 
@@ -59,17 +59,17 @@ ifneq ($(CONFIG_SPL_BUILD),y)
 obj-$(CONFIG_IMX_BOOTAUX) += imx_bootaux.o
 endif
 obj-$(CONFIG_SATA) += sata.o
-obj-$(CONFIG_IMX_HAB)    += hab.o
+obj-$(CONFIG_$(SPL_)IMX_HAB)    += hab.o
 obj-$(CONFIG_SYSCOUNTER_TIMER) += syscounter.o
 obj-$(CONFIG_IMX_TRUSTY_OS) += trusty.o
 endif
 ifeq ($(SOC),$(filter $(SOC),mx7ulp))
 obj-y  += cache.o mmdc_size.o
-obj-$(CONFIG_IMX_HAB) += hab.o
+obj-$(CONFIG_$(SPL_)IMX_HAB) += hab.o
 endif
 ifneq ($(CONFIG_SPL_BUILD),y)
 ifeq ($(SOC),$(filter $(SOC),mx6 mx7ulp imx8m))
-obj-$(CONFIG_IMX_HAB) += fiohab.o
+obj-$(CONFIG_$(SPL_)IMX_HAB) += fiohab.o
 endif
 ifeq ($(SOC),$(filter $(SOC),imx8))
 obj-$(CONFIG_AHAB_BOOT) += fiohab.o

commit d1fa209729acef21cdd2f12d2968b7bef9865ae9

[FIO tosquash] arch: arm: mach-imx: fix building error without SPL_IMX_HUB
The hab functions shouldn't be called if SPL_IMX_HAB is disabled. It
fixes the building error [1].

[1]
| ld: cc6xCyeo.ltrans0.ltrans.o: in function `spl_sdp_load_image':
| arch/arm/mach-imx/spl.c:437: undefined reference to `imx_hab_is_enabled'

Fixes: adf2fc975a3 ("[FIO toup] arch: mach-imx: hab: add SPL_IMX_HAB config")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

arch/arm/mach-imx/spl.c
@@ -417,7 +417,7 @@ void *spl_load_simple_fit_fix_load(const void *fit)
 	unsigned long size;
 	u8 *tmp = (u8 *)fit;
 
-	if (IS_ENABLED(CONFIG_IMX_HAB)) {
+	if (IS_ENABLED(CONFIG_SPL_IMX_HAB)) {
 		if (IS_ENABLED(CONFIG_IMX_SPL_FIT_FDT_SIGNATURE)) {
 			u32 offset = ALIGN(fdt_totalsize(fit), 0x1000);
 
@@ -533,7 +533,7 @@ int board_spl_fit_post_load(const void *fit, struct spl_image_info *spl_image)
 	int ret;
 #endif
 
-	if (CONFIG_IS_ENABLED(IMX_HAB) && !(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD)) {
+	if (IS_ENABLED(CONFIG_SPL_IMX_HAB) && !(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD)) {
 		u32 offset = ALIGN(fdt_totalsize(fit), 0x1000);
 
 		if (imx_hab_authenticate_image((uintptr_t)fit,